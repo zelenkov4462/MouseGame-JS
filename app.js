993d482e3dd299e2a9b124e2904d5aed10e4dae0
Nikita Zelenkov <zelenkov4462@gmail.com>
2022-10-24 10:12:44 +0700

    edit eventListener

--- app.js
+++ app.js
@@ -6,26 +6,38 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
 
-    square.addEventListener('mouseover', () => {
-        setColor(square)
-    })
+    // square.addEventListener('mouseover', () => {
+    //     setColor(square)
+    // })
+
+    square.addEventListener('mouseover', setColor)
 
     square.addEventListener('mouseleave', () => {
 
-            removeColor(square)
-
+        removeColor(square)
     })
+
+
 
     board.append(square)
 
 }
+function printColor(element) {
+    element.style.backgroundColor= getRandomColor()
+}
 
 
-function setColor(element) {
+// function setColor(element) {
+//     const color = getRandomColor()
+//     element.style.backgroundColor = color
+//     element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+// }
+
+function setColor(event) {
+    const element = event.target
     const color = getRandomColor()
     element.style.backgroundColor = color
     element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
-
 }
 
 function removeColor(element) {
